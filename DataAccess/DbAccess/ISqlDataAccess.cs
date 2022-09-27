d613cb6582424da3ec67a16eaf458b19b6682618
Дмитрий Корец <dimon.korecz@gmail.com>
2022-09-27 14:42:09 +0300

    Add project files.

---
+++ DataAccess/DbAccess/ISqlDataAccess.cs
@@ -0,0 +1,7 @@
+﻿namespace DataAccess.DbAccess;
+
+public interface ISqlDataAccess
+{
+    Task<IEnumerable<T>> Query<T, U>(string storedProcedure, U parameters, string connectionId = "Default");
+    Task Command<T>(string storedProcedure, T parameters, string connectionId = "Default");
+}
---
+++ UserDB/Script.PostDeployment.sql
@@ -0,0 +1,8 @@
+﻿--Demo Data
+if not exists (select 1 from dbo.[User])
+begin
+	 insert into dbo.[User] (FirstName, LastName)
+	 values ('Dima','Korets'),
+	 ('Sasha','Bogomazova'),
+	 ('Vladimir','Bobkov');
+end
